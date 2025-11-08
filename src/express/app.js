3d4047ff1e41732466fcefce9f7949f66bdbef69
Denis Albertini <denis.s.albertini@gmail.com>
2025-11-08 20:45:35 -0300

    update express app

--- src/express/app.js
+++ src/express/app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { errorHandlingMiddleware } from './middlewares/error-handling-middleware.js';
+import { openapiValidatorMiddleware } from './middlewares/openapi-valiator-middleware.js';
 import apiRouter from './routers/api-router.js';
 
 const app = express();
 
 app.use(express.json());
+app.use(openapiValidatorMiddleware);
 app.use('/api', apiRouter);
 app.use(errorHandlingMiddleware);
 

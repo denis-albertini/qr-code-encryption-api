1011daff6d6e650bfe742b92345c3c1321d26155
Denis Albertini <denis.s.albertini@gmail.com>
2025-11-08 16:54:13 -0300

    update qr code model

--- src/models/qr-code.js
+++ src/models/qr-code.js
@@ -1,28 +1,29 @@
-import { DataTypes } from 'sequelize';
-import sequelize from '../sequelize.js';
+import { DataTypes, Model } from 'sequelize';
 
-const QRCode = sequelize.define(
-  'QRCode',
-  {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true,
-    },
-    content: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    signature: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  },
-  {
-    tableName: 'qr_code',
-    paranoid: true,
-    createdAt: true,
+export default class QRCode extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        id: {
+          type: DataTypes.UUID,
+          defaultValue: DataTypes.UUIDV4,
+          primaryKey: true,
+        },
+        content: {
+          type: DataTypes.TEXT,
+          allowNull: false,
+        },
+        signature: {
+          type: DataTypes.CHAR(344),
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        tableName: 'qr_code',
+        paranoid: true,
+        createdAt: true,
+      }
+    );
   }
-);
-
-export default QRCode;
+}

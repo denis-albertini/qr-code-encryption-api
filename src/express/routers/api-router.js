69b8f9c50597449c9756b6ce642e365e32615795
Denis Albertini <denis.s.albertini@gmail.com>
2025-11-08 20:45:08 -0300

    update api router

--- src/express/routers/api-router.js
+++ src/express/routers/api-router.js
@@ -1,12 +1,30 @@
 import express from 'express';
 import complaintRouter from './complaint-router.js';
+import docRouter from './doc-router.js';
 import qrCodeRouter from './qr-code-router.js';
 import userRouter from './user-router.js';
 
 const router = express.Router();
 
-router.use('/users', userRouter);
-router.use('/qr-codes', qrCodeRouter);
-router.use('/complaints', complaintRouter);
+router.use(
+  '/users',
+  userRouter
+  // #swagger.tags = ['Users']
+);
+router.use(
+  '/qr-codes',
+  qrCodeRouter
+  // #swagger.tags = ['QRCodes']
+);
+router.use(
+  '/complaints',
+  complaintRouter
+  // #swagger.tags = ['Complaints']
+);
+router.use(
+  '/doc',
+  docRouter
+  // #swagger.ignore = true
+);
 
 export default router;

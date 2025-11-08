438801d7891082ddbba45e376bce0b8e711db97c
Denis Albertini <denis.s.albertini@gmail.com>
2025-11-08 16:54:20 -0300

    update complaint model

--- src/models/complaint.js
+++ src/models/complaint.js
@@ -1,16 +1,28 @@
-import { DataTypes } from 'sequelize';
-import sequelize from '../sequelize.js';
+import { DataTypes, Model } from 'sequelize';
 
-const Complaint = sequelize.define(
-  'Complaint',
-  {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true,
-    },
-  },
-  { tableName: 'complaint' }
-);
-
-export default Complaint;
+export default class Complaint extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        id: {
+          type: DataTypes.UUID,
+          defaultValue: DataTypes.UUIDV4,
+          primaryKey: true,
+        },
+        description: {
+          type: DataTypes.STRING(500),
+        },
+        deviceId: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          unique: true,
+        },
+      },
+      {
+        sequelize,
+        tableName: 'complaint',
+        createdAt: true,
+      }
+    );
+  }
+}
